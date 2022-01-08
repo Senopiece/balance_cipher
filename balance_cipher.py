8f270fe28af3d191ce2eb3f95c8b6b8a22346722
Senopiece <shichiromail@gmail.com>
2022-01-09 02:51:35 +0300

    first rns encode realisation

---
+++ balance_cipher.py
@@ -0,0 +1,25 @@
+from f100pb_rns import f100pb_rns as RNS
+
+# sender secret
+balance1 = 454532
+secret1 = 45677
+
+# sender open data (all the calculations are performed privately)
+M1 = RNS(secret1)
+cb1 = M1*RNS(balance1)
+cbn1 = M1*RNS(balance1 - 40)
+
+# recipient secret
+balance2 = 345654
+secret2 = 34325
+
+# recipient open data (all the calculations are performed privately)
+M2 = RNS(secret2)
+cb2 = M2*RNS(balance2)
+cbn2 = M2*RNS(balance2 + 40)
+
+# verification procedure
+# TODO: prevent underflow
+print((M2*cb1 + M1*cb2) == (M2*cbn1 + M1*cbn2))
+print(M2*cb1 + M1*cb2)
+print(M2*cbn1 + M1*cbn2)
---
+++ rns.py
@@ -0,0 +1,85 @@
+class ComposedNumber:
+    _data = None
+    _basis = None
+    _basis_mul = None
+
+
+    def __init__(self, basis):
+        self._data = 0
+        self._basis = basis # <- final
+
+        # slow at the creation, takes more memory, but efficient in calculations
+        self._basis_mul = [1]
+        for e in basis:
+            self._basis_mul.append(self._basis_mul[-1] * e)
+        # self._basis_mul <- final
+
+
+    def __getitem__(self, i):
+        return (self._data // self._basis_mul[i]) % self._basis[i]
+    
+
+    def __setitem__(self, i, v):
+        assert 0 <= v < self._basis[i]
+        delta = v - self[i]
+        self._data += delta * self._basis_mul[i]
+    
+
+    def __eq__(self, other):
+        assert isinstance(other, ComposedNumber)
+        return self._data == other._data
+    
+
+    def __str__(self):
+        decomposition = []
+        for i in range(len(self._basis)):
+            decomposition.append(self[i])
+        return f'[raw: {self._data}], decomposition: {tuple(decomposition)}'
+
+
+class RNS:
+    _data : ComposedNumber = None
+
+    def __init__(self, num, basis=None):
+        assert (isinstance(num, int) and isinstance(basis, list)) or (isinstance(num, ComposedNumber) and basis == None), \
+            "constructor parameter must be a integer to convert or a raw data (ComposedNumber)"
+        if isinstance(num, int):
+            decomposition = ComposedNumber(basis)
+            for i, e in enumerate(basis):
+                decomposition[i] = num % e
+            num = decomposition
+        self._data = num
+    
+
+    def __add__(self, other):
+        assert isinstance(other, RNS)
+        assert self._data._basis == other._data._basis
+
+        local_data = ComposedNumber(self._data._basis)
+
+        for i in range(len(self._data._basis)):
+            local_data[i] = (self._data[i] + other._data[i]) % self._data._basis[i]
+
+        return RNS(local_data)
+    
+
+    def __mul__(self, other):
+        assert isinstance(other, RNS)
+        assert self._data._basis == other._data._basis
+
+        local_data = ComposedNumber(self._data._basis)
+
+        for i in range(len(self._data._basis)):
+            local_data[i] = (self._data[i] * other._data[i]) % self._data._basis[i]
+
+        return RNS(local_data)
+
+    
+    def __eq__(self, other):
+        assert isinstance(other, RNS)
+        assert self._data._basis == other._data._basis
+        return other._data == self._data # delegates to ComposedNumber == ComposedNumber
+    
+
+    def __str__(self):
+        return str(self._data) # delegates to str(ComposedNumber)
