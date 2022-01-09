5435eeb24b2582951644a5a7ae940c7d41fd8413
Senopiece <shichiromail@gmail.com>
2022-01-09 11:50:31 +0300

    more about protocol

--- balance_cipher.py
+++ balance_cipher.py
@@ -18,8 +18,20 @@ M2 = RNS(secret2)
 cb2 = M2*RNS(balance2)
 cbn2 = M2*RNS(balance2 + 40)
 
-# verification procedure
+# account registration procedure (usually comes with the first transaction)
+# 1) account owner sends its value M
+# 2) before binding this M with the account public address we need to check that:
+# 2.1) M != RNS(0)
+# 2.2) there is no other M binded with this account
+# 3) that M binds with this account public address on the blockchain
+
+# transaction verification procedure
 # TODO: prevent underflow
+
+# 1) M1 and M2 exists
+# 2) there is no underflow
+# 3) (M2*cb1 + M1*cb2) == (M2*cbn1 + M1*cbn2)
+
 print((M2*cb1 + M1*cb2) == (M2*cbn1 + M1*cbn2))
 print(M2*cb1 + M1*cb2)
 print(M2*cbn1 + M1*cbn2)
