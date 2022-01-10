b2e479fba82181695658247de91d9b637aa9f3ad
Senopiece <shichiromail@gmail.com>
2022-01-11 01:27:07 +0300

    no more rns

--- balance_cipher.py
+++ balance_cipher.py
@@ -1,55 +1,62 @@
-from f100pb_rns import f100pb_rns as RNS
-import time
+module = 2**256
 
-# common open data
-mask = tuple(1 < n < 44 for n in range(100)) # mask from f100pb to f3t44pb
+
+def atomic_add(x, n):
+    # where x is in the domain and n is just a scalar to add
+    return (pow(529, n, module))*x % module
+
+
+def encode(x):
+    # where x is just a scalar to be represented in the domain
+    return atomic_add(1, x)
+
+
+def decode(x):
+    # where x is in the domain
+    # NOTE: it is good here to be such slow
+    # TODO: check discrete log for this
+    res = 0
+    while x != 1:
+        x = atomic_add(x, -1)
+        res += 1
+    return res
+
+
+def add(a, b):
+    # where a and b are both in the domain
+    return (a * b) % module
+    # works as
+    # while b != origin: # usually origin = 1
+    #     b = atomic_add(b, -1)
+    #     a = atomic_add(a, 1)
+    # return a
+
+
+def mul(a, n):
+    # where a is in domain and n is just a number (scalar multiplication)
+    return pow(a, n, module)
+
 
 # sender secret
-balance1 = 454532
-M1 = RNS(456773424327589256738493263542374632787842342)
+balance1 = 50
 
 # sender open data (all the calculations are performed privately)
-M1s = (M1*M1).mask(mask)
-cb1 = (M1*RNS(balance1)).mask(mask)
-cbn1 = (M1*RNS(balance1 - 40)).mask(mask)
-
-from math import sqrt
-print(cb1.decode() / sqrt(M1s.decode())) # <- the only way to hack (works only if M1 is less than multiple of f3t44pb)
+M1 = 40858891157460307674354882010572547498271232277915032737235431902522015964929
+cb1 = encode(M1*balance1)
+cb1n = encode(M1*(balance1 - 40))
 
 # recipient secret
-balance2 = 345654
-M2 = RNS(343257245492657294437562934328998262378492489)
+balance2 = 3456
 
 # recipient open data (all the calculations are performed privately)
-M2s = (M2*M2).mask(mask)
-cb2 = (M2*RNS(balance2)).mask(mask)
-cbn2 = (M2*RNS(balance2 + 40)).mask(mask)
+M2 = 105378189937130164065839937598184558310536310992898322397669330185205034255393
+cb2 = encode(M2*balance2)
+cb2n = encode(M2*(balance2 + 40))
 
-# account registration procedure (usually comes with the first transaction)
-# 1) account owner sends its value Ms
-# 2) before binding this Ms with the account public address we need to check that:
-# 2.1) Ms != RNS(0)
-# 2.2) there is no other Ms binded with this account
-# 3) finally bind the Ms with this account on the blockchain
-
-# transaction verification procedure
-# TODO: prevent underflow
-
-# 1) M1s and M2s exists
-# 2) M2s*(cb1 - cbn1)^2 == M1s*(cb2 - cbn2)^2
-
-a = time.time()
-
-d1 = cb1 - cbn1
-d2 = cb2 - cbn2
-
-l = M2s*d1*d1
-r = M1s*d2*d2
+# verification procedure
+l = add(mul(cb2n, M1), mul(cb1n, M2))
+r = add(mul(cb2, M1), mul(cb1, M2))
 
 print(f"l = {l}")
 print(f"r = {r}")
 print("l == r ✓" if l == r else "l != r")
-
-b = time.time()
-print("Verification time: ", b - a)
-
