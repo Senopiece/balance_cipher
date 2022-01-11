c18ea19096f237e3dba0156b49d65368fb6cb4a2
Senopiece <shichiromail@gmail.com>
2022-01-11 16:22:53 +0300

    add class Crypted

--- balance_cipher.py
+++ balance_cipher.py
@@ -23,57 +23,60 @@ def decode(x):
     return res
 
 
-def add(a, b):
-    # where a and b are both in the domain
-    return (a * b) % module
-    # works as
-    # while b != origin: # usually origin = 1
-    #     b = atomic_add(b, -1)
-    #     a = atomic_add(a, 1)
-    # return a
+class Crypted:
+    _data = None
 
-
-def mul(a, n):
-    # where a is in domain and n is just a number (scalar multiplication)
-    return pow(a, n, module)
+    def __init__(self, n=0):
+        self._data = encode(n)
+    
+    def __add__(self, other):
+        assert isinstance(other, Crypted)
+        res = Crypted()
+        res._data = (self._data * other._data) % module
+        return res
+    
+    def __sub__(self, other):
+        assert isinstance(other, Crypted)
+        res = Crypted()
+        res._data = (self._data * pow(other._data, -1, module)) % module
+        return res
+    
+    def __mul__(self, n):
+        assert isinstance(n, int) # scalar mul
+        res = Crypted()
+        res._data = pow(self._data, n, module)
+        return res
+    
+    def __eq__(self, other):
+        assert isinstance(other, Crypted)
+        return other._data == self._data
+    
+    def __str__(self):
+        return str(self._data)
 
 
 # sender secret
 balance1 = 60
 
 # sender open data (all the calculations are performed privately)
-M1 = 40858891157460307674354882010572547498271232277915032737235431902522015964929
-M1n = 1203174766785810276710872305903520022234420437434091757055991933319522845286
-cb1 = encode(M1*balance1)
-cb1n = encode(M1n*(balance1 - 40))
+m1 = 61831996350531240199057085283232358026495125261252625916218455899905125740353
+m2 = 77347096829997860251145238741378512327304242939718028116032409255917948845697
+A1 = Crypted(m1*balance1)
+A2 = Crypted(m2*(balance1 - 40))
 
 # recipient secret
 balance2 = 3456
 
 # recipient open data (all the calculations are performed privately)
-M2 = 105378189937130164065839937598184558310536310992898322397669330185205034255393
-M2n = 71209169186837707306666995366925534503437113810227523981093294414235915606017
-cb2 = encode(M2*balance2)
-cb2n = encode(M2n*(balance2 + 40))
+g1 = 42334496918515581520281029467747373373690656937939313516048188657552755383937
+g2 = 97680493710316016549179486752524137080160327319815402918319565371107429039937
+B1 = Crypted(g1*balance2)
+B2 = Crypted(g2*(balance2 + 40))
 
 # verification
-def is_positive(x, m):
-    neg = encode(-m)
-    for _ in range(10):
-        if neg == x:
-            return False
-        neg = atomic_add(neg, -m)
-    return True
-    
-l = mul(add(mul(cb2n, M1n), mul(cb1n, M2n)), M1*M2)
-r = mul(add(mul(cb2, M1), mul(cb1, M2)), M1n*M2n)
+l = (B2*m2 + A2*g2)*m1*g1
+r = (B1*m1 + A1*g1)*m2*g2
 
-
-print(f"sign: cb1n[{'+' if is_positive(cb1n, M1n) else '-'}] cb2n[{'+' if is_positive(cb2n, M2n) else '-'}] ")
 print(f"l = {l}")
 print(f"r = {r}")
 print("l == r ✓" if l == r else "l != r")
-
-print(-7 % 30)
-print((-7 % 30)**2 % 30)
-print((7)**2 % 30)
